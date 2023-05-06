b95d8c60ace1e7c1b1bf0d77a2d55e9372b20712
Hildhan123 <41043524+Hildhan123@users.noreply.github.com>
2023-05-06 18:50:15 +0700

    Update sql

--- routes/record.js
+++ routes/record.js
@@ -11,10 +11,10 @@ const multer = Multer({
 
 // TODO: Sesuaikan konfigurasi database
 const connection = mysql.createConnection({
-    host: 'public_ip_sql_instance_Anda',
+    host: '34.128.88.50',
     user: 'root',
-    database: 'nama_database_Anda',
-    password: 'password_sql_Anda'
+    database: 'money-tracker-sql',
+    password: 'lol123lol'
 })
 
 router.get("/dashboard", (req, res) => {
